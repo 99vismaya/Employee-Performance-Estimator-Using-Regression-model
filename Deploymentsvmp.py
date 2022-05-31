07977bc43a6d4ccf6ea1e4716f6b771ab0d9c396
99vismaya <106010576+99vismaya@users.noreply.github.com>
2022-05-31 09:43:47 +0530

    Update Deploymentsvmp.py

--- Deploymentsvmp.py
+++ Deploymentsvmp.py
@@ -5,6 +5,14 @@ Created on Thu Feb 17 20:22:26 2022
 @author: ADMIN
 """
 
+echo "# streamlit-to-heroku-tutorial" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin master
+
 import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
