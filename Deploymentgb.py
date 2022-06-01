e23a56e48ee1fe767c8b297eac987fbe37e51327
99vismaya <106010576+99vismaya@users.noreply.github.com>
2022-06-01 18:24:54 +0530

    Update Deploymentgb.py

--- Deploymentgb.py
+++ Deploymentgb.py
@@ -4,6 +4,13 @@ Created on Sun Feb 13 18:45:38 2022
 
 @author: ADMIN
 """
+echo "# streamlit-to-heroku-tutorial" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/your_username/your_repo_name.git
+git push -u origin master
 
 import streamlit as st
 import pandas as pd
