382efc94628ff1ed6ac28be5802b7c3f9e1f0c6b
99vismaya <106010576+99vismaya@users.noreply.github.com>
2022-06-03 18:02:33 +0530

    Update Deploymentgb.py

--- Deploymentgb.py
+++ Deploymentgb.py
@@ -9,7 +9,7 @@ git init
 git add README.md
 git commit -m "first commit"
 git branch -M master
-git remote add origin https://github.com/your_username/your_repo_name.git
+git remote add origin https://github.com/99vismaya/kisan.git
 git push -u origin master
 
 import streamlit as st
